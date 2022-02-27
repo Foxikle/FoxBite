2989c51d21524704562a881a9a2c01f839595a81
BuildTools <unconfigured@null.spigotmc.org>
2022-02-27 12:18:12 -0500

    Initial commit

---
+++ src/main/java/me/foxikle/foxbite/FoxBite.java
@@ -0,0 +1,17 @@
+package me.foxikle.foxbite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FoxBite extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
